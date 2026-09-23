14fb69a10f03c3ee7de578fe0a50132aac744798
DigitalWorldPure <digitalworldpure@gmail.com>
2026-09-23 15:45:21 +0300

    Missing Scaffold added

--- app/src/main/java/com/goldenankh/purewave/ui/screens/TracksScreen.kt
+++ app/src/main/java/com/goldenankh/purewave/ui/screens/TracksScreen.kt
@@ -24,13 +24,22 @@ THE SOFTWARE.
 
 package com.goldenankh.purewave.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.goldenankh.purewave.ui.components.tracklist.LazyTracksDemo
 
 @Composable
 fun TracksScreen() {
-    LazyTracksDemo()
+    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            LazyTracksDemo()
+        }
+    }
 }
 
 @Preview(showBackground = true)
